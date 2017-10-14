d7dd563227dd8eee31d07e52cf365febae294f6d
Mike Waddell <waddellmusic@gmail.com>
2017-10-14 20:22:10 +0000

    Collaborators work

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171014195946) do
+  
+  create_table "collaborators", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "wiki_id"
+  end
+
+  add_index "collaborators", ["user_id"], name: "index_collaborators_on_user_id"
+  add_index "collaborators", ["wiki_id"], name: "index_collaborators_on_wiki_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
